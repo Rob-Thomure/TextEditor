2aed928b694a4d821b6c036ff299e6e7c6483dad
Rob Thomure <robthomure@outlook.com>
2022-10-12 10:58:40 -0500

    Stage 3: added a menu bar.

--- src/main/java/org/example/TextEditor.java
+++ src/main/java/org/example/TextEditor.java
@@ -2,6 +2,7 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class TextEditor extends JFrame {
     private final JTextField filenameField;
@@ -15,6 +16,28 @@ public class TextEditor extends JFrame {
         setTitle("Text editor");
         setLocationRelativeTo(null);
 
+        // Menu Bar
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setName("MenuFile");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        menuBar.add(fileMenu);
+
+        JMenuItem loadFileMenuItem = new JMenuItem("Load");
+        loadFileMenuItem.setName("MenuLoad");
+        JMenuItem saveFileMenuItem = new JMenuItem("Save");
+        saveFileMenuItem.setName("MenuSave");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.setName("MenuExit");
+
+        fileMenu.add(loadFileMenuItem);
+        fileMenu.add(saveFileMenuItem);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMenuItem);
+
+        // Border Layout North
         JPanel filePanel = new JPanel();
         add(filePanel, BorderLayout.NORTH);
 
@@ -27,21 +50,35 @@ public class TextEditor extends JFrame {
         saveButton.setName("SaveButton");
         saveButton.setPreferredSize(new Dimension(65, 20));
         filePanel.add(saveButton);
-        saveButton.addActionListener(e -> textEditorController.saveFile(filenameField, textArea));
 
         JButton loadButton = new JButton("Load");
         loadButton.setName("LoadButton");
         loadButton.setPreferredSize(new Dimension(65, 20));
         filePanel.add(loadButton);
-        loadButton.addActionListener(e -> textEditorController.loadFile(filenameField, textArea));
 
+        // Border Layout South
         textArea = new JTextArea();
         textArea.setName("TextArea");
         JScrollPane scrollableTextArea = new JScrollPane(textArea);
         scrollableTextArea.setName("ScrollPane");
         scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        add(scrollableTextArea);
+        add(scrollableTextArea, BorderLayout.CENTER);
+
+        // ActionListeners
+        loadFileMenuItem.addActionListener(e -> textEditorController.loadFile(filenameField, textArea));
+        saveFileMenuItem.addActionListener(e -> textEditorController.saveFile(filenameField, textArea));
+
+        exitMenuItem.addActionListener(e -> {
+            dispose();
+            System.exit(0);
+        });
+
+        saveButton.addActionListener(e -> textEditorController.saveFile(filenameField, textArea));
+        loadButton.addActionListener(e -> textEditorController.loadFile(filenameField, textArea));
+
+
+
         setVisible(true);
     }
 }
